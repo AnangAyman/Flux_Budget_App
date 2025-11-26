bb4c6d2d2cfe870009e355e1460b0912023ac6a7
Ayman <anang.ayman@gmail.com>
2025-11-26 12:43:19 +0700

    create transaction table and update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'preferred_language'
     ];
 
     /**
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relationship: One User has Many Transactions
+    public function transactions() {
+        return $this->hasMany(Transaction::class);
     }
 }
